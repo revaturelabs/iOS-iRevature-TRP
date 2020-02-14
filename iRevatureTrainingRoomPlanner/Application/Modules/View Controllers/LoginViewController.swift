be00cf2969460ab638370892edf302c1fb7f83b5
WesOtero <wesleyotero@protonmail.com>
2020-02-14 14:02:41 -0500

    UI Progress, Login Authrntication

--- iRevatureTrainingRoomPlanner/Application/Modules/View Controllers/LoginViewController.swift
+++ iRevatureTrainingRoomPlanner/Application/Modules/View Controllers/LoginViewController.swift
@@ -5,25 +5,40 @@
 //  Created by admin on 2/13/20.
 //  Copyright © 2020 admin. All rights reserved.
 //
-
 import Foundation
 import UIKit
 
-class LogoutViewController: UIViewController{
+class LoginViewController: UIViewController{
     
+    var userName: String = "Wes"
+    var password: String = "pass"
     
-    @IBAction func actionLogOut(_ sender: Any) {
-        logOut()
+    @IBOutlet weak var textFieldEmail: UITextField!
+    
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
-    func logOut(){
-        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    @IBAction func actionLogin(_ sender: Any) {
         
-        guard let mainNavigationVC = mainStoryboard.instantiateViewController(withIdentifier: "startViewController") as? UIViewController else {
-            return
-        }
-        mainNavigationVC.modalPresentationStyle = UIModalPresentationStyle.fullScreen
-        present(mainNavigationVC, animated: true, completion: nil)
+        navigateToLanding(email: textFieldEmail.text ?? "", pass: textFieldPassword.text ?? "")
     }
     
+    private func navigateToLanding(email: String, pass: String){
+        if(userName == email && password == pass){
+            
+            let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            
+            guard let mainNavigationVC = mainStoryboard.instantiateViewController(withIdentifier: "LandingPage") as? UIViewController else {
+                return
+            }
+            mainNavigationVC.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+            present(mainNavigationVC, animated: true, completion: nil)
+            
+        } else {
+            print("Not logged in")
+        }
+    }
 }
