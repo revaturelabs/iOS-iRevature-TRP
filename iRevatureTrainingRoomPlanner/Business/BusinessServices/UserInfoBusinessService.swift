c94ea7a2eb8e70616734894fffeee687384b4c62
WesOtero <wesleyotero@protonmail.com>
2020-02-18 11:52:02 -0500

    Add UserInfoBusinessService and UserInfoProtocol

---
+++ iRevatureTrainingRoomPlanner/Business/BusinessServices/UserInfoBusinessService.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfoBusinessService.swift
+//  iRevatureTrainingRoomPlanner
+//
+//  Created by admin on 2/18/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
