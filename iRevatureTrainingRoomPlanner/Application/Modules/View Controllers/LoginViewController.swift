7e3bf756aff8fab5be79dc069ca6201fd3f28e17
WesOtero <wesleyotero@protonmail.com>
2020-02-18 18:14:51 -0500

    "Connect IBOutle to LoginController"

--- iRevatureTrainingRoomPlanner/Application/Modules/View Controllers/LoginViewController.swift
+++ iRevatureTrainingRoomPlanner/Application/Modules/View Controllers/LoginViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class LoginViewController: UIViewController{
     
-//    var user = User(username: "Wes", password: "pass")
-    
-    
-    
     @IBOutlet weak var textFieldEmail: UITextField!
     @IBOutlet weak var textFieldPassword: UITextField!
     @IBOutlet weak var labelInvalidUsernamePassword: UILabel!
+    @IBOutlet weak var switchKeepMeSignedIn: UISwitch!
     
     var userInfoBusinessService: UserInfoBusinessService = UserInfoBusinessService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(switchKeepMeSignedIn.isOn)
         if let currentUserInfo: User = userInfoBusinessService.getUserInfo(){
             let email = currentUserInfo.email
+            print(email)
             textFieldEmail.text = email
         }
         
@@ -45,19 +43,23 @@ class LoginViewController: UIViewController{
     private func authenticateUser(username: String, password: String){
         
         #warning("uncomment this line to perform the check on user wants to be remebered or not")
-        //let userKeepMeLoggedIn = switchKeepMeLoggedIn
+        let userKeepMeSignedIn = switchKeepMeSignedIn
         
         if(username == "test@revature.com" && password == "test1"){
             
             let userInfoData: User = User(firstName: "test", lastName: "", email: username, token: "", keepLoggedIn: true)
             
             userInfoBusinessService.setUserInfo(userObject: userInfoData)
-             let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            
+            //Swgues to the next story board
+            let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
             
             guard let mainNavigationVC = mainStoryboard.instantiateViewController(withIdentifier: "LandingPage") as? UIViewController else {
                 return
             }
+            
             navigationController?.pushViewController(mainNavigationVC, animated: true)
+            
             labelInvalidUsernamePassword.isHidden = true
             
             //TODO: log this
@@ -66,20 +68,5 @@ class LoginViewController: UIViewController{
             //TODO: log this
         }
         
-//        if(user.username == username && user.password == password){
-//
-//            let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-//
-//            guard let mainNavigationVC = mainStoryboard.instantiateViewController(withIdentifier: "LandingPage") as? UIViewController else {
-//                return
-//            }
-//            navigationController?.pushViewController(mainNavigationVC, animated: true)
-//            labelInvalidUsernamePassword.isHidden = true
-//
-//            //TODO: log this
-//        } else {
-//            labelInvalidUsernamePassword.isHidden = false
-//            //TODO: log this
-//        }
     }
 }
