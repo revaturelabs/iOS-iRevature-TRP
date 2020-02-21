7e05e0e8362bdf67bd5766dce395c4a6f9aa2d37
empty <empty>
2020-02-21 15:22:31 -0500

    Add DatabseOperations openDatabse Method

--- iRevatureTrainingRoomPlanner/Persistence/DatabaseOperations.swift
+++ iRevatureTrainingRoomPlanner/Persistence/DatabaseOperations.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+struct iRevatureTables {
+    
+    static var userTable: SQLiteTable {
+
+    var userTable = SQLiteTable(tableName: "user")
+
+    //Adds columns to the instance of table 'user'
+    userTable.addColumn(columnName: "user_id", dataType: .INT, constraints: .PRIMARYKEY, .NOTNULL)
+
+    userTable.addColumn(columnName: "first_name", dataType: .CHAR, constraints: .NOTNULL)
+
+    userTable.addColumn(columnName: "last_name", dataType: .CHAR, constraints: .NOTNULL)
+
+    userTable.addColumn(columnName: "email", dataType: .CHAR, constraints: .NOTNULL)
+
+    userTable.addColumn(columnName: "keep_logged_in", dataType: .INT, constraints: .none)
+
+    userTable.addColumn(columnName: "token", dataType: .CHAR, constraints: .none)
+
+    userTable.addColumn(columnName: "user_role", dataType: .CHAR, constraints: .NOTNULL)
+
+    return userTable
+    
+    }
+    
+}
+
+class DatanseOperations{
+        
+    func openDatabse(){
+        
+        let filePath = DatabaseAccess.getDatabaseFilePath(name: "iRevatureTrainer", pathDirectory: .documentDirectory, domainMask: .userDomainMask)
+        
+        //Open Databse
+        let db = DatabaseAccess.openDatabase(path: filePath, createIfDoesNotExist: true)
+        
+    }
+    
+}
