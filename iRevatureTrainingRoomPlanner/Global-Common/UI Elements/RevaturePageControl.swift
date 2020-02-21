e87ceaf4fe5408e66b39b9cebecbb1f58e8c3e6c
jmalisse <jeremy.malisse@gmail.com>
2020-02-20 23:00:30 -0500

    Rebase/Refactor of entire project after IDE conflicts (Thursday, Feb 20, 2020). When in doubt confirm with each other about everything. Spelling errors, file management issues, or incorrect usage/placement/import of files can and will continue to cause problems. Following commit will restore the project to working order - sans the SQL functionality, which will follow.

--- iRevatureTrainingRoomPlanner/Global-Common/UI Elements/RevaturePageControl.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PageControl.swift
-//  iRevatureTrainingRoomPlanner
-//
-//  Created by admin on 2/17/20.
-//  Copyright © 2020 admin. All rights reserved.
-//
-
-import Foundation
--- iRevatureTrainingRoomPlanner/Services/API/RestAlamodeFireManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RestAlamodeFireManager.swift
-//  iRevatureTrainingRoomPlanner
-//
-//  Created by admin on 2/19/20.
-//  Copyright © 2020 admin. All rights reserved.
-//
-
-import Foundation
