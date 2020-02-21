71517627c8838fb0a2af9727d8a78c33ebe84757
empty <empty>
2020-02-21 15:50:51 -0500

    Updates to UserDBOperations, design for Trainer table in DatabseOperations

--- iRevatureTrainingRoomPlanner/Persistence/DatabaseOperations.swift
+++ iRevatureTrainingRoomPlanner/Persistence/DatabaseOperations.swift
@@ -11,32 +11,57 @@ import Foundation
 struct iRevatureTables {
     
     static var userTable: SQLiteTable {
-
-    var userTable = SQLiteTable(tableName: "user")
-
-    //Adds columns to the instance of table 'user'
-    userTable.addColumn(columnName: "user_id", dataType: .INT, constraints: .PRIMARYKEY, .NOTNULL)
-
-    userTable.addColumn(columnName: "first_name", dataType: .CHAR, constraints: .NOTNULL)
-
-    userTable.addColumn(columnName: "last_name", dataType: .CHAR, constraints: .NOTNULL)
-
-    userTable.addColumn(columnName: "email", dataType: .CHAR, constraints: .NOTNULL)
-
-    userTable.addColumn(columnName: "keep_logged_in", dataType: .INT, constraints: .none)
-
-    userTable.addColumn(columnName: "token", dataType: .CHAR, constraints: .none)
-
-    userTable.addColumn(columnName: "user_role", dataType: .CHAR, constraints: .NOTNULL)
-
-    return userTable
+        
+        var userTable = SQLiteTable(tableName: "user")
+        
+        //Adds columns to the instance of table 'user'
+        userTable.addColumn(columnName: "user_id", dataType: .CHAR, constraints: .PRIMARYKEY, .NOTNULL)
+        
+        userTable.addColumn(columnName: "first_name", dataType: .CHAR, constraints: .NOTNULL)
+        
+        userTable.addColumn(columnName: "last_name", dataType: .CHAR, constraints: .NOTNULL)
+        
+        userTable.addColumn(columnName: "email", dataType: .CHAR, constraints: .NOTNULL)
+            
+        userTable.addColumn(columnName: "user_role", dataType: .CHAR, constraints: .NOTNULL)
+        
+        userTable.addColumn(columnName: "token", dataType: .CHAR, constraints: nil)
+        
+        userTable.addColumn(columnName: "keep_logged_in", dataType: .BOOL, constraints: nil)
+        
+        return userTable
+        
+    }
     
+    
+    static var trainerTable: SQLiteTable {
+        
+        var userTable = SQLiteTable(tableName: "trainer")
+        
+        //Adds columns to the instance of table 'trainer'
+        //Some contrainsts have been left blank for flexibility
+        userTable.addColumn(columnName: "trainer_id", dataType: .CHAR, constraints: .PRIMARYKEY, .NOTNULL)
+        
+        userTable.addColumn(columnName: "first_name", dataType: .CHAR, constraints: .NOTNULL)
+        
+        userTable.addColumn(columnName: "last_name", dataType: .CHAR, constraints: .NOTNULL)
+        
+        userTable.addColumn(columnName: "email", dataType: .CHAR, constraints: nil)
+                
+        userTable.addColumn(columnName: "phone_number", dataType: .CHAR, constraints: nil)
+        
+        userTable.addColumn(columnName: "base_location", dataType: .CHAR, constraints: nil)
+        
+        userTable.addColumn(columnName: "slack_username", dataType: .CHAR, constraints: nil)
+        
+        return userTable
+        
     }
     
 }
 
-class DatanseOperations{
-        
+class DatabaseOperations{
+    
     func openDatabse(){
         
         let filePath = DatabaseAccess.getDatabaseFilePath(name: "iRevatureTrainer", pathDirectory: .documentDirectory, domainMask: .userDomainMask)
--- iRevatureTrainingRoomPlanner/Persistence/UserDBOperations.swift
+++ iRevatureTrainingRoomPlanner/Persistence/UserDBOperations.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+extension DatabaseOperations{
+    
+    func createUserTable(databse: DatabaseAccess){
+        
+        //Grabs a new instance of usertables based on iRevatureTables struct
+        if let userTable = iRevatureTables.userTable.makeStatement(){
+            debugPrint(userTable)
+        }
+        
+        //Tries to executes the SQLite3 Prepared Statement
+        do{
+            
+            try databse.createTable(table: iRevatureTables.userTable.self)
+            
+        } catch{
+            
+            debugPrint("Oops! a problem occurred with: \(userTable)")
+            
+        }
+        
+        func insertUserRecord()
+        
+        
+        
+    }
+    
+}
