1f18534c1a656d2db0475eb256cc54ecb325972d
empty <empty>
2020-02-25 19:47:01 -0500

    Add junction table DBOperation files

---
+++ iRevatureTrainingRoomPlanner/Persistence/BatchSkillDBOperations.swift
@@ -0,0 +1,9 @@
+//
+//  BatchSkillDBOperations.swift
+//  iRevatureTrainingRoomPlanner
+//
+//  Created by admin on 2/25/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
---
+++ iRevatureTrainingRoomPlanner/Persistence/RoomAvailabilityDBOperations.swift
@@ -0,0 +1,9 @@
+//
+//  RoomAvailabilityDBOperations.swift
+//  iRevatureTrainingRoomPlanner
+//
+//  Created by admin on 2/25/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
